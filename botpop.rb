954f84f4ae42de8576ca686a9d301545b205b56f
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-05-06 19:48:36 +0200

    Yolo ca marche et c'est bien sale.
    - J'en ai rien a faire de faire des vrai commits

--- botpop.rb
+++ botpop.rb
@@ -17,10 +17,13 @@ class Botpop
   SEARCH_ENGINES_KEYS = SEARCH_ENGINES.keys.map{|e|"!"+e}.join(', ')
   SEARCH_ENGINES_HELP = SEARCH_ENGINES.keys.map{|e|"!"+e+" [search]"}.join(', ')
 
+  VERSION = IO.read('version')
+  TARGET = /[[:alnum:]_\-\.]+/
+
   Dir[File.expand_path "plugins/*.rb"].each{|f| require_relative(f)}
-  BotpopPlugins::constants.each do |youknowwhatimeanplug|
-    prepend BotpopPlugins::const_get(youknowwhatimeanplug)
-  end
+  # BotpopPlugins::constants.each do |youknowwhatimeanplug|
+  #   prepend BotpopPlugins::const_get(youknowwhatimeanplug)
+  # end
 
   def start
     @engine.start
@@ -45,21 +48,21 @@ class Botpop
         m.reply url
       end
 
-      on :message, /\!(#{SEARCH_ENGINES.keys.join('|')}) .+/ do |m| exec_search m end
-      on :message, /!troll .+/ do |m| exec_troll m end
-      on :message, "!version" do |m| exec_version m end
-      on :message, "!code" do |m| exec_code m end
-      on :message, "!intra" do |m| exec_intra m end
-      on :message, "!intra on" do |m| exec_intra_on m end
-      on :message, "!intra off" do |m| exec_intra_off m end
-      on :message, "!ping" do |m| exec_ping m end
-      on :message, /!ping #doTARGETend\Z/ do |m| exec_ping_target m end
-      on :message, /!dos #doTARGETend\Z/ do |m| exec_dos m end
-      on :message, /!fok #doTARGETend\Z/ do |m| exec_fok m end
-      on :message, /!trace #doTARGETend\Z/ do |m| exec_trace m end
-      on :message, /!poke #doTARGETend\Z/ do |m| exec_poke m end
-      on :message, "!cmds" do |m| exec_help m end
-      on :message, "!help" do |m| exec_help m end
+      on :message, /\!(#{SEARCH_ENGINES.keys.join('|')}) .+/ do |m| BotpopPlugins::exec_search m end
+      on :message, /!troll .+/ do |m| BotpopPlugins::exec_troll m end
+      on :message, "!version" do |m| BotpopPlugins::exec_version m end
+      on :message, "!code" do |m| BotpopPlugins::exec_code m end
+      on :message, "!intra" do |m| BotpopPlugins::exec_intra m end
+      on :message, "!intra on" do |m| BotpopPlugins::exec_intra_on m end
+      on :message, "!intra off" do |m| BotpopPlugins::exec_intra_off m end
+      on :message, "!ping" do |m| BotpopPlugins::exec_ping m end
+      on :message, /!ping #{TARGET}\Z/ do |m| BotpopPlugins::exec_ping_target m end
+      on :message, /!dos #{TARGET}\Z/ do |m| BotpopPlugins::exec_dos m end
+      on :message, /!fok #{TARGET}\Z/ do |m| BotpopPlugins::exec_fok m end
+      on :message, /!trace #{TARGET}\Z/ do |m| BotpopPlugins::exec_trace m end
+      on :message, /!poke #{TARGET}\Z/ do |m| BotpopPlugins::exec_poke m end
+      on :message, "!cmds" do |m| BotpopPlugins::exec_help m end
+      on :message, "!help" do |m| BotpopPlugins::exec_help m end
 
     end
   end
--- plugins/base.rb
+++ plugins/base.rb
@@ -1,41 +1,37 @@
 #encoding: utf-8
 
 module BotpopPlugins
-  module Base
 
-    VERSION = IO.read('version')
-    TARGET = /[[:alnum:]_\-\.]+/
-
-    def get_msg m
-      URI.encode(m.params[1..-1].join(' ').gsub(/\![^ ]+ /, ''))
-    end
-
-    def get_ip m
-      m.params[1..-1].join(' ').gsub(/\![^ ]+ /, '').gsub(/[^[:alnum:]\-\_\.]/, '')
-    end
-
-    def help m
-      m.reply "!cmds, !help, !version, !code, !dos [ip], !fok [nick], !ping, !ping [ip], !trace [ip], !poke [nick], !troll [msg], !intra, !intra [on/off], #{SEARCH_ENGINES_HELP}"
-    end
-
-    def exec_version m
-      m.reply VERSION
-    end
-
-    def exec_code m
-      m.reply "https://github.com/pouleta/botpop"
-    end
-
-    def exec_help
-      help m
-    end
-
-    def exec_troll m
-      # hours = (Time.now.to_i - Time.gm(2015, 04, 27, 9).to_i) / 60 / 60
-      s = get_msg m
-      url = "http://www.fuck-you-internet.com/delivery.php?text=#{s}"
-      m.reply url
-    end
-
+  def self.get_msg m
+    URI.encode(m.params[1..-1].join(' ').gsub(/\![^ ]+ /, ''))
   end
+
+  def self.get_ip m
+    m.params[1..-1].join(' ').gsub(/\![^ ]+ /, '').gsub(/[^[:alnum:]\-\_\.]/, '')
+  end
+
+  # This is the most creepy and ugly method ever see
+  def self.help m
+    m.reply "!cmds, !help, !version, !code, !dos [ip], !fok [nick], !ping, !ping [ip], !trace [ip], !poke [nick], !troll [msg], !intra, !intra [on/off], #{Botpop::SEARCH_ENGINES_HELP}"
+  end
+
+  def self.exec_version m
+    m.reply Botpop::VERSION
+  end
+
+  def self.exec_code m
+    m.reply "https://github.com/pouleta/botpop"
+  end
+
+  def self.exec_help m
+    help m
+  end
+
+  def self.exec_troll m
+    # hours = (Time.now.to_i - Time.gm(2015, 04, 27, 9).to_i) / 60 / 60
+    s = get_msg m
+    url = "http://www.fuck-you-internet.com/delivery.php?text=#{s}"
+    m.reply url
+  end
+
 end
