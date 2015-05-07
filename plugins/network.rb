549f169a5b1917714e6a07d68c1bddc34bec7ddc
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-05-07 19:08:02 +0200

    Large improvement of the network code quality (factorization, ...)

--- plugins/network.rb
+++ plugins/network.rb
@@ -6,7 +6,7 @@ module BotpopPlugins
     m.reply Action.intra_state rescue m.reply "I'm buggy. Sorry"
   end
 
-  INTRA_PING_SLEEP = 1 #30
+  INTRA_PING_SLEEP = 30
   def self.exec_intra_on m
     @intra ||= Mutex.new
     if @intra.try_lock
@@ -50,15 +50,18 @@ module BotpopPlugins
   def self.exec_dos m
     @dos ||= Mutex.new
     if @dos.try_lock
+      ip = get_ip m
       begin
-        ip = get_ip m
         if not Action.ping(ip)
           m.reply "Cannot reach the host '#{ip}'"
           raise "Unreachable host"
         end
         m.reply "Begin attack against #{ip}"
+        # Calculations
         s = Action.dos(ip, DOS_DURATION).split("\n")[3].to_s
+        # Display
         m.reply (Action.ping(ip) ? "failed :(" : "down !!!") + " " + s
+        # Wait a little before the next authorized attack
         sleep DOS_WAIT
         @dos.unlock
       rescue
@@ -84,23 +87,43 @@ module BotpopPlugins
     m.reply "#{nick} : " + (Action.ping(ip) ? "failed :(" : "down !!!") + " " + s
   end
 
+  # Trace is complexe. 3 functions used exec_trace_display_lines, exec_trace_with_time, exec_trace
+  TRACE_DURATION_INIT = 0.3
+  TRACE_DURATION_INCR = 0.1
+  def self.exec_trace_display_lines m, lines
+    lines.select!{|e| not e.include? "no reply" and e =~ /\A \d+: .+/}
+    duration = TRACE_DURATION_INIT
+    lines.each do |l|
+      m.reply l
+      sleep duration
+      duration += TRACE_DURATION_INCR
+    end
+    m.reply 'finished'
+  end
+
+  def self.exec_trace_with_time ip
+    t1 = Time.now
+    s = Action.trace ip
+    t2 = Time.now
+    return [s, t1, t2]
+  end
+
   def self.exec_trace m
     @trace ||= Mutex.new
     if @trace.try_lock
+      ip = get_ip m
+      m.reply "It can take time"
       begin
-        ip = get_ip m
-        m.reply "It can take time"
-        t1 = Time.now; s = Action.trace ip; t2 = Time.now
-        m.reply "Used #{(t2 - t1).round(3)} seconds"
-        so = s.select{|e| not e.include? "no reply" and e =~ /\A \d+: .+/}
+        # Calculations
+        s, t1, t2 = exec_trace_with_time ip
+        m.reply "Trace executed in #{(t2 - t1).round(3)} seconds"
         @trace.unlock
-        duration = 0.3
-        so.each{|l| m.reply l; sleep duration; duration += 0.1}
-        m.reply "Trace #{ip} done"
       rescue => e
         m.reply "Sorry, but the last author of this plugin is so stupid his mother is a tomato"
         @trace.unlock
       end
+      # Display
+      exec_trace_display_lines m, s
     else
       m.reply "Please retry after when the last trace end"
     end
@@ -110,12 +133,11 @@ module BotpopPlugins
     nick = get_ip_from_nick(m)[:nick]
     ip = get_ip_from_nick(m)[:ip]
     return m.reply "User '#{nick}' doesn't exists" if ip.nil?
+    # Calculations
     p = Net::Ping::External.new ip
-    str = "failed"
-    if p.ping?
-      str = "#{(p.duration*100.0).round 2}ms (#{p.host})"
-    end
-    m.reply "#{nick} poke> #{str}"
+    # Display
+    response = p.ping? ? "#{(p.duration*100.0).round 2}ms (#{p.host})" : "failed"
+    m.reply "#{nick} poke> #{response}"
   end
 
 end
