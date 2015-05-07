4cd38629fe1eb9a44743251a4b50cf629ff40af8
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-05-07 17:27:04 +0200

    Fix coupons to stay compatible with other plugins by renaming constants

--- plugins/coupons.rb
+++ plugins/coupons.rb
@@ -6,30 +6,25 @@ require 'json'
 
 module BotpopPlugins
 
-  CONFIG = YAML.load_file('plugins/coupon_login.yml')['creditentials']
+  COUPON_CONFIG = YAML.load_file('plugins/coupon_login.yml')['creditentials']
+  COUPON_USER = COUPON_CONFIG['username']
+  COUPON_PASS = COUPON_CONFIG['password']
+  COUPON_ORGA = COUPON_CONFIG['organisation']
+  COUPON_APIU = COUPON_CONFIG['api_coupon_url']
+  COUPON_URL = URI(COUPON_APIU)
 
-  USER = CONFIG['username']
-  PASS = CONFIG['password']
-  ORGA = CONFIG['organisation']
-  APIU = CONFIG['api_coupon_url']
-  URL = URI(APIU)
   def self.exec_coupon m
     coupon = m.params[1..-1].join(' ').gsub(/(coupon:)/, '').split.first
     coupon = coupon.gsub(/[^A-z0-9\.\-_]/, '') # secure a little
-
     begin
-      # req = Net::HTTP::Get.new(URL, {coupon: coupon, organisation: ORGA})
-      # req.basic_auth USER, PASS
-
-      http = Net::HTTP.new(URL.host, URL.port)
+      http = Net::HTTP.new(COUPON_URL.host, COUPON_URL.port)
       http.use_ssl = true
       # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Post.new(URL)
+      request = Net::HTTP::Post.new(COUPON_URL)
       request.add_field('Content-Type', 'application/json')
-      request.basic_auth USER, PASS
-      request.body = {'coupon' => coupon, 'organisation' => ORGA}.to_json
+      request.basic_auth COUPON_USER, COUPON_PASS
+      request.body = {'coupon' => coupon, 'organisation' => COUPON_ORGA}.to_json
       response = http.request(request)
-
       # `curl https://api.pathwar.net/organization-coupons/#{coupon} -u '#{USER}:#{PASS}' -X GET`
       m.reply "#{coupon} " + (response.code == '200' ? 'validated' : "failed with #{response.code}")
     rescue => e
