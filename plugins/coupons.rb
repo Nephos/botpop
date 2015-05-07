098461f2536d2fa52b85b474e793c3682f7b18ae
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-05-07 17:05:56 +0200

    Fix coupons

--- plugins/coupons.rb
+++ plugins/coupons.rb
@@ -1,6 +1,8 @@
 #encoding: utf-8
 
-require 'mechanize'
+require 'uri'
+require 'net/http'
+require 'json'
 
 module BotpopPlugins
 
@@ -8,23 +10,29 @@ module BotpopPlugins
 
   USER = CONFIG['username']
   PASS = CONFIG['password']
-  URL = "https://api.pathwar.net/organization-coupons/___COUPON___"
+  ORGA = CONFIG['organisation']
+  APIU = CONFIG['api_coupon_url']
+  URL = URI(APIU)
   def self.exec_coupon m
     coupon = m.params[1..-1].join(' ').gsub(/(coupon:)/, '').split.first
     coupon = coupon.gsub(/[^A-z0-9\.\-_]/, '') # secure a little
 
     begin
-      uri = URI(URL.gsub('___COUPON___', coupon))
-      req = Net::HTTP::Get.new(uri)
-      req.basic_auth USER, PASS
-      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') {|http|
-        @rep = http.request(req)
-      }
+      # req = Net::HTTP::Get.new(URL, {coupon: coupon, organisation: ORGA})
+      # req.basic_auth USER, PASS
+
+      http = Net::HTTP.new(URL.host, URL.port)
+      http.use_ssl = true
+      # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Post.new(URL)
+      request.add_field('Content-Type', 'application/json')
+      request.basic_auth USER, PASS
+      request.body = {'coupon' => coupon, 'organisation' => ORGA}.to_json
+      response = http.request(request)
 
       # `curl https://api.pathwar.net/organization-coupons/#{coupon} -u '#{USER}:#{PASS}' -X GET`
-      STDERR.puts @rep.body
-      m.reply "#{coupon} " + (@rep.code == '200' ? 'validated' : 'missed')
-    rescue
+      m.reply "#{coupon} " + (response.code == '200' ? 'validated' : "failed with #{response.code}")
+    rescue => e
       m.reply "#{coupon} buggy"
     end
   end
