a5495fe2ff60bd3dd6a12acdb5c7cbba02785feb
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-08-11 23:57:02 +0200

    add "taggle" plugin

---
+++ plugins/taggle.rb
@@ -0,0 +1,26 @@
+#encoding: utf-8
+
+class Taggle
+
+  match(/!tg (.+)/, use_prefix: false, method: :exec_tg)
+
+  HELP = ["!tg [nick]"]
+  CONFIG = Botpop::CONFIG['taggle'] || {} # || raise(MissingConfigurationZone, self.to_s)
+  ENABLED = CONFIG['enable'].nil? ? true : CONFIG['enable'] rescue true
+  NTIMES = CONFIG['ntimes'] || 10
+  WAIT = CONFIG['wait'] || 0.3
+
+  def exec_tg c, m, who
+    @@tg_lock ||= Mutex.new
+    @@tg_lock.lock
+    begin
+      NTIMES.times do
+        c.User(who).send("tg #{who}")
+        sleep WAIT
+      end
+    ensure
+      @@tg_lock.unlock
+    end
+  end
+
+end
