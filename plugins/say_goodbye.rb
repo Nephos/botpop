3c537008fe25eeea989fa7a98130588828825b3b
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-08-11 23:40:08 +0200

    add plugin "say_goodbye"

---
+++ plugins/say_goodbye.rb
@@ -0,0 +1,15 @@
+#encoding: utf-8
+
+class SayGoodBye
+  match(/^!sg [\w\-\.].+/, use_prefix: false, method: :exec_sg)
+
+  HELP = ["!sg src_name"]
+  CONFIG = Botpop::CONFIG['say_goodbye'] || raise(MissingConfigurationZone, self.name)
+  ENABLED = CONFIG['enable'].nil? ? true : CONFIG['enable']
+
+  def exec_sg m
+    arg = m.message.split.last
+    m.reply CONFIG[arg].shuffle.first
+  end
+
+end
