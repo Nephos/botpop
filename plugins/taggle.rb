9e67ec32359014d7d8a656c77dc975a40e8fcde2
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-08-12 00:20:52 +0200

    fix taggle

--- plugins/taggle.rb
+++ plugins/taggle.rb
@@ -1,11 +1,12 @@
 #encoding: utf-8
 
 class Taggle
+  include Cinch::Plugin
 
   match(/!tg (.+)/, use_prefix: false, method: :exec_tg)
 
   HELP = ["!tg [nick]"]
-  CONFIG = Botpop::CONFIG['taggle'] || {} # || raise(MissingConfigurationZone, self.to_s)
+  CONFIG = Botpop::CONFIG['taggle'] || {} || raise(MissingConfigurationZone, self.to_s)
   ENABLED = CONFIG['enable'].nil? ? true : CONFIG['enable'] rescue true
   NTIMES = CONFIG['ntimes'] || 10
   WAIT = CONFIG['wait'] || 0.3
