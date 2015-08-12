2a5b786f26d01a83f57676edc9cb72e66af7bd3f
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-08-12 13:56:52 +0200

    add "searchable" plugin

---
+++ plugins/searchable.rb
@@ -0,0 +1,21 @@
+#encoding: utf-8
+
+class Searchable < BotpopPlugin
+  include Cinch::Plugin
+
+  CONFIG = Botpop::CONFIG['searchable'] || raise(MissingConfigurationZone, self.to_s)
+  ENABLED = CONFIG['enable'].nil? ? true : CONFIG['enable']
+
+  VALUES = CONFIG.values.map{|e|"!"+e}.join(', ')
+  KEYS = CONFIG.keys.map{|e|"!"+e}.join(', ')
+  HELP = CONFIG.keys.map{|e|"!"+e+" [search]"}
+  match(/\!(#{CONFIG.keys.join('|')}) .+/, use_prefix: false, method: :exec_search)
+
+  def exec_search m
+    msg = BotpopBuiltins.get_msg m
+    url = CONFIG[m.params[1..-1].join(' ').gsub(/\!([^ ]+) .+/, '\1')]
+    url = url.gsub('___MSG___', msg)
+    m.reply url
+  end
+
+end
