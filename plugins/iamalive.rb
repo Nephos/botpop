a4abe968628193456fb42462d1be0af39c242af8
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-08-12 18:29:21 +0200

    improve "iamalive" plugin
    - the bot can learn sentence
    - the bot can react to a sentence

--- plugins/iamalive.rb
+++ plugins/iamalive.rb
@@ -3,9 +3,18 @@ class IAmAlive < Botpop::Plugin
 
   match(/^[^!].*/, use_prefix: false, method: :register_entry)
   match(/^[^!].*/, use_prefix: false, method: :react_on_entry)
+  match(/^!iaa reac(tivity)?$/, use_prefix: false, method: :get_reactivity)
+  match(/^!iaa reac(tivity)? \d{1,3}$/, use_prefix: false, method: :set_reactivity)
+  match(/^!iaa learn$/, use_prefix: false, method: :set_mode_learn)
+  match(/^!iaa live$/, use_prefix: false, method: :set_mode_live)
+  match(/^!iaa mode$/, use_prefix: false, method: :get_mode)
+
   CONFIG = config(:safe => true)
   ENABLED = CONFIG['enable'] || false
   DATABASE_FILE = (Dir.pwd + "/plugins/iamalive/" + (CONFIG['database'] || "db.sqlite3"))
+
+  @@mode = :learn
+  @@reactivity = config['reactivity'] || 50
 
   if ENABLED
     require 'sequel'
@@ -18,6 +27,34 @@ class IAmAlive < Botpop::Plugin
   end
 
   def react_on_entry m
+    return if @@mode != :live
+    e = Entry.where(message: m.message).to_a.map(&:id).map{|x| x+1}
+    if rand(1..100) > @@reactivity
+      a = Entry.where(id: e).to_a.shuffle.first
+      return if a.nil?
+      m.reply a.message
+      Entry.create(user: "self", message: a.message)
+    end
+  end
+
+  def get_reactivity m
+    m.reply "Current reactivity: #{@@reactivity}"
+  end
+
+  def set_reactivity m
+    @@reactivity = m.message.split[2].to_i
+  end
+
+  def set_mode_learn
+    @@mode = :learn
+  end
+
+  def set_mode_live
+    @@mode = :live
+  end
+
+  def get_mode
+    m.reply "Current mode: #{@@mode}"
   end
 
 end
