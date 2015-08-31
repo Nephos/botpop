3d5e33c638c0672caa20f52aefd7f07a0148806f
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-08-31 02:06:15 +0200

    fix searchable

--- plugins/searchable.rb
+++ plugins/searchable.rb
@@ -12,7 +12,7 @@ class Searchable < Botpop::Plugin
   match(/\!(#{config.keys.join('|')}) .+/, use_prefix: false, method: :exec_search)
 
   def exec_search m
-    msg = BotpopBuiltins.get_msg m
+    msg = Builtins.get_msg m
     url = CONFIG[m.params[1..-1].join(' ').gsub(/\!([^ ]+) .+/, '\1')]
     url = url.gsub('___MSG___', msg)
     m.reply url
