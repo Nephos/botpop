24b520d7024759c492bea0dae979ef8ec896bcb9
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-09-18 23:13:48 +0200

    add cequetudisnaaucunsens

---
+++ plugins/cequetudisnaaucunsens.rb
@@ -0,0 +1,14 @@
+class CeQueTuDisNAAucunSens < Botpop::Plugin
+  include Cinch::Plugin
+
+  match(/!?what/, use_prefix: false, method: :say_random)
+
+  HELP = ["!what"]
+  ENABLED = config['enable'].nil? ? false : config['enable']
+  CONFIG = config
+
+  def say_random m
+    m.reply %w(ce que tu dis n'a aucun sens).shuffle.join(' ')
+  end
+
+end
