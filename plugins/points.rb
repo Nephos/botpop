e16c9b9c65ce5bd33b7a585e0c353f5f5595eec6
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-10-02 17:46:01 +0200

    improve points with stats

--- plugins/points.rb
+++ plugins/points.rb
@@ -5,15 +5,27 @@ class Points < Botpop::Plugin
   include Botpop::Plugin::Database
 
   match(/.*/, use_prefix: false, method: :save_last_user)
+  match(/!p-stats$/, use_prefix: false, method: :statistics)
+  match(/!p-stats (\w+)$/, use_prefix: false, method: :statistics_for)
   match(/!p +(\w+)$/, use_prefix: false, method: :add_point_to_last)
   match(/!p +(\w+) +(\w+)$/, use_prefix: false, method: :add_point_to_user)
 
-  HELP = ["!p [type] <to>"]
+  HELP = ["!p <type> [to]", "!p-stats [user]"]
   ENABLED = config['enable'].nil? ? true : config['enable']
   CONFIG = config
 
   @@users = {}
   @@lock = Mutex.new
+
+  def statistics m
+    ret = Base::DB.fetch("SELECT points.type, COUNT(points.*) AS nb FROM points GROUP BY points.type ORDER BY nb DESC LIMIT 10;").all.map{|e| e[:type] + "(#{e[:nb]})"}.join(", ")
+    m.reply "Top used: #{ret}"
+  end
+
+  def statistics_for m, u
+    ret = Base::DB.fetch("SELECT points.type, COUNT(points.*) AS nb FROM points WHERE assigned_to = ? GROUP BY points.type", u.downcase).all.map{|e| e[:type] + "(#{e[:nb]})"}.join(", ")
+    m.reply "User #{u} has: #{ret}"
+  end
 
   def save_last_user m
     return if m.message.match(/^!p .+$/)
