44e98fb7e9f15d48d543f918c828b1bed61dc7af
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-10-12 01:04:11 +0200

    improve plugin points with 2 commands

--- plugins/points.rb
+++ plugins/points.rb
@@ -5,12 +5,13 @@ class Points < Botpop::Plugin
   include Botpop::Plugin::Database
 
   match(/.*/, use_prefix: false, method: :save_last_user)
-  match(/^!p-stats$/, use_prefix: false, method: :statistics)
-  match(/^!p-stats (\w+)$/, use_prefix: false, method: :statistics_for)
+  match(/^!pstats$/, use_prefix: false, method: :statistics)
+  match(/^!pstats?u (\w+)$/, use_prefix: false, method: :statistics_for_user)
+  match(/^!pstats?p (\w+)$/, use_prefix: false, method: :statistics_for_point)
   match(/^!p +(\w+)$/, use_prefix: false, method: :add_point_to_last)
   match(/^!p +(\w+) +(\w+)$/, use_prefix: false, method: :add_point_to_user)
 
-  HELP = ["!p <type> [to]", "!p-stats [user]"]
+  HELP = ["!p <type> [to]", "!pstats", "!pstatsu <nick>", "!pstatsp <point>"]
   ENABLED = config['enable'].nil? ? true : config['enable']
   CONFIG = config
 
@@ -18,16 +19,22 @@ class Points < Botpop::Plugin
   @@lock = Mutex.new
 
   def statistics m
-    # ret = Base::DB.fetch("SELECT points.type, COUNT(points.*) AS nb FROM points GROUP BY points.type ORDER BY nb DESC LIMIT 10;").all.map{|e| e[:type] + "(#{e[:nb]})"}.join(", ")
-    data = Base::DB.fetch("SELECT points.type, COUNT(points.*) AS nb FROM points GROUP BY points.type ORDER BY nb DESC LIMIT 10;").all
-    data.map!{|e| Base::DB.fetch("SELECT assigned_to FROM points GROUP BY type, assigned_to HAVING type = ? ORDER BY COUNT(*) DESC;", e[:type]).first.merge(e)}
-    ret = data.map{|e| e[:type] + "(#{e[:nb]}x #{e[:assigned_to]})"}.join(", ")
+    ret = Base::DB.fetch("SELECT points.type, COUNT(points.*) AS nb FROM points GROUP BY points.type ORDER BY nb DESC LIMIT 10;").all.map{|e| e[:type] + "(#{e[:nb]})"}.join(", ")
+    # data = Base::DB.fetch("SELECT points.type, COUNT(points.*) AS nb FROM points GROUP BY points.type ORDER BY nb DESC LIMIT 10;").all
+    # data.map!{|e| Base::DB.fetch("SELECT assigned_to FROM points GROUP BY type, assigned_to HAVING type = ? ORDER BY COUNT(*) DESC;", e[:type]).first.merge(e)}
+    # ret = data.map{|e| e[:type] + "(#{e[:nb]}x #{e[:assigned_to]})"}.join(", ")
     m.reply "Top used: #{ret}"
   end
 
-  def statistics_for m, u
-    ret = Base::DB.fetch("SELECT points.type, COUNT(points.*) AS nb FROM points WHERE assigned_to = ? GROUP BY points.type", u.downcase).all.map{|e| e[:type] + "(#{e[:nb]})"}.join(", ")
+  def statistics_for_user m, u
+    ret = Base::DB.fetch("SELECT points.type, COUNT(points.*) AS nb FROM points WHERE assigned_to = ? GROUP BY points.type LIMIT 10;", u.downcase).all.map{|e| e[:type] + "(#{e[:nb]})"}.join(", ")
     m.reply "User #{u} has: #{ret}"
+  end
+
+  def statistics_for_point m, p
+    data = Base::DB.fetch("SELECT assigned_to, COUNT(*) AS nb FROM points GROUP BY type, assigned_to HAVING type = ? ORDER BY COUNT(*) DESC LIMIT 10;", p).all
+    ret = data.map{|e| e[:assigned_to] + "(#{e[:nb]})"}.join(", ")
+    m.reply "Point #{p}: #{ret}"
   end
 
   def save_last_user m
